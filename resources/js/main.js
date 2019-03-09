12d911eb8c401162bc7cee8df9ea62cc555ae778
Jordan Heasman <jconheasman@gmail.com>
2019-03-09 21:05:34 +0000

    seventh commmit. added slick cdn. cannot achieve carousel

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,6 +1,27 @@
 $(document).ready(function(){
-  $('.jumbotron').slick({
-    setting-name: setting-value
+  $('.carousel').slick({
+    autoplay:true,
+    slidesToScroll:1,
+    speed:4000,
+    arrows:false,
+    draggable:false,
+    pauseOnFocus:false,
+    pauseOnHover:false
   });
 });
-	
+
+// below is copied from the code of example site //
+// currently not functioning as navbar is not fixed // 
+var lastScrollTop = 0;
+$(window).scroll(function(){
+  var scrollTop = $(this).scrollTop();
+  if (scrollTop - lastScrollTop > 50){
+    var navHeight = $('.navbar').css('height');
+    $('.navbar').animate({top: '-' + navHeight}, 150);
+    lastScrollTop = scrollTop;
+  } else if (lastScrollTop - scrollTop > 50) {
+    $('.navbar').animate({top: '0px'}, 150);
+    lastScrollTop = scrollTop;
+  }
+ });
+});
